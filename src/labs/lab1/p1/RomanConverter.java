948a2fb7c6d7e4ef7c460a31379451245abf50cd
Doozy <slobodan.kletnikov@gmail.com>
2016-10-08 12:00:38 +0200

    1

---
+++ src/labs/lab1/p1/RomanConverter.java
@@ -0,0 +1,16 @@
+package labs.lab1.p1;
+
+class RomanConverter {
+    /**
+     * Roman to decimal converter
+     *
+     * @param n number in decimal format
+     * @return string representation of the number in Roman numeral
+     */
+     static String toRoman(int n) {
+        // your solution here
+
+        return "";
+    }
+
+}
